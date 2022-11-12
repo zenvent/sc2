2bc084a1ec1381fd941afc4d357bb643598d9d6d
Zac <zac@zenvent.com>
2022-11-12 12:53:10 -0500

    New Theme

--- sc2.js
+++
@@ -1,8 +0,0 @@
-
-
-function showTable(id){
-	document.getElementById('ptable').style.display = "none";
-	document.getElementById('ttable').style.display = "none";
-	document.getElementById('ztable').style.display = "none";			
-	document.getElementById(id).style.display = "inline-block";
-}
