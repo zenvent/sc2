1aa754bb0cd68f2800f2fa97a3be0e6371d1f2e0
Zac <zac@zenvent.com>
2022-11-12 23:28:28 -0500

    Fixed Header Buttons

--- sc2.js
+++ sc2.js
@@ -1,8 +1,17 @@
 function showTable(tableName){
+    // Hide all tables
     const boxes = document.getElementsByTagName('table');
     for (const box of boxes) {
         box.style.display = 'none';
     }
+    // Show one table
     document.getElementById(tableName).style.display="table";
+    
+    // Remove effect from all buttons
+    const logos = document.getElementsByClassName('logo-active');
+    for (const logo of logos) {
+        logo.classList.remove('logo-active');
+    }
+    // Enable one button effect
     document.getElementById(tableName+"-logo").classList.add('logo-active');
 }
